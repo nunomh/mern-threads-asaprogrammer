23af998ab9bd0302fe870dee1d4fd717fed19d9e
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-02-03 15:51:46 +0000

    PostPage layout changes and Comment component

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,7 +1,11 @@
-import { Avatar, Flex, Img, Text } from '@chakra-ui/react';
+import { Avatar, Flex, Img, Text, Box, Image, Divider, Button } from '@chakra-ui/react';
 import { BsThreeDots } from 'react-icons/bs';
+import PostActions from '../components/PostActions';
+import { useState } from 'react';
+import Comment from '../components/Comment';
 
 const PostPage = () => {
+    const [liked, setLiked] = useState(false);
     return (
         <>
             <Flex>
@@ -21,6 +25,36 @@ const PostPage = () => {
                     <BsThreeDots />
                 </Flex>
             </Flex>
+            <Text my={3}>Let&apos;s talk about threads</Text>
+            <Box borderRadius={6} overflow={'hidden'} border={'1px solid'} borderColor={'gray.light'}>
+                <Image src={'/post1.png'} w={'full'} />
+            </Box>
+            <Flex gap={3} my={3}>
+                <PostActions liked={liked} setLiked={setLiked} />
+            </Flex>
+            <Flex gap={2} alignItems={'center'}>
+                <Text color={'gray.light'} fontSize={'sm'}>
+                    238 replies
+                </Text>
+                <Box w={0.5} h={0.5} borderRadius={'full'} bg={'gray.light'}></Box>
+                <Text color={'gray.light'} fontSize={'sm'}>
+                    {200 + (liked ? 1 : 0)} likes
+                </Text>
+            </Flex>
+            <Divider my={4} />
+
+            <Flex justifyContent={'space-between'}>
+                <Flex gap={2} alignItems={'center'}>
+                    <Text fontSize={'2xl'}>👋</Text>
+                    <Text color={'gray.light'}>Get the app to like, reply and post.</Text>
+                </Flex>
+                <Button>Get</Button>
+            </Flex>
+
+            <Divider my={4} />
+            <Comment />
+            <Comment />
+            <Comment />
         </>
     );
 };
