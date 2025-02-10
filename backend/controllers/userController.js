514a138ed70528ee094c51d8ad3d3fd146be9214
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-02-10 11:45:49 +0000

    API: SignIn endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,37 @@
 import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
 
 const signupUser = async (req, res) => {
     try {
         const { name, username, email, password } = req.body;
 
         const user = await User.findOne({ $or: [{ email }, { username }] });
+
+        if (user) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            name,
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+        if (newUser) {
+            res.status(201).json({
+                _id: newUser._id,
+                name: newUser.name,
+                username: newUser.username,
+                email: newUser.email,
+            });
+        } else {
+            res.status(400).json({ error: 'Invalid user data' });
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
         console.log('Error in signupUser: ', error.message);
