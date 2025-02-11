097d5717f4fef2d7857bef8e68e898d0c2c7be0a
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-02-11 17:47:13 +0000

    API: Add update endpoint and start working on profile endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,19 @@ import User from '../models/userModel.js';
 import bcrypt from 'bcrypt';
 import generateTokenAndSetCookie from '../utils/helpers/generateTokenAndSetCookie.js';
 
+// Get user profile
+const getUserProfile = async (req, res) => {
+    try {
+        const username = req.params.username;
+        const user = await User.findOne({ username }).select('-password').select('-updatedAt');
+        if (!user) return res.status(404).json({ error: 'User not found' });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        console.log('Error in getUserProfile: ', error.message);
+    }
+};
+
 // Signup user
 const signupUser = async (req, res) => {
     try {
@@ -88,7 +101,8 @@ const followUnfollowUser = async (req, res) => {
         const currentUser = await User.findById(req.user._id);
 
         if (!currentUser || !userToModify) return res.status(404).json({ error: 'User not found' });
-        if (id === req.user._id) return res.status(400).json({ error: 'You cannot follow/unfollow yourself' });
+        if (id === req.user._id.toString())
+            return res.status(400).json({ error: 'You cannot follow/unfollow yourself' });
 
         if (currentUser.following.includes(id)) {
             // Unfollow user
@@ -109,4 +123,51 @@ const followUnfollowUser = async (req, res) => {
     }
 };
 
-export { signupUser, loginUser, logoutUser, followUnfollowUser };
+// Update user
+const updateUser = async (req, res) => {
+    try {
+        const { name, username, email, password, profilePicture, bio } = req.body;
+
+        const userId = req.user._id;
+
+        let user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        if (req.params.id !== userId.toString()) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const checkIfUsernameExists = await User.findOne({ username });
+        if (checkIfUsernameExists) {
+            return res.status(400).json({ error: 'Username already exists' });
+        }
+        const checkIfEmailExists = await User.findOne({ email });
+        if (checkIfEmailExists) {
+            return res.status(400).json({ error: 'Email already exists' });
+        }
+
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            user.password = hashedPassword;
+        }
+
+        user.name = name || user.name;
+        user.username = username || user.username;
+        user.email = email || user.email;
+        user.profilePic = profilePicture || user.profilePic;
+        user.bio = bio || user.bio;
+
+        await user.save();
+
+        res.status(200).json({ message: 'User updated successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        console.log('Error in updateUser: ', error.message);
+    }
+};
+
+export { signupUser, loginUser, logoutUser, followUnfollowUser, updateUser, getUserProfile };
