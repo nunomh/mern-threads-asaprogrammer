b1e42d898c515be0a7496c2bef2bffd28798ec12
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-02-12 17:11:04 +0000

    API: Get, delete, like and unlike post endpoints

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import Post from '../models/postModel.js';
 
+// Create post
 const createPost = async (req, res) => {
     try {
         const { postedBy, text, img } = req.body;
@@ -39,4 +40,65 @@ const createPost = async (req, res) => {
     }
 };
 
-export { createPost };
+// Get post
+const getPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        console.log('Error in getPost: ', error.message);
+    }
+};
+
+// Delete post
+const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        if (post.postedBy.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ error: 'Unauthorized to delete post' });
+        }
+
+        await post.deleteOne();
+        res.status(200).json({ message: 'Post deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        console.log('Error in deletePost: ', error.message);
+    }
+};
+
+// Like Unlike post
+const likeUnlikePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        if (post.likes.includes(req.user._id)) {
+            // Unlike post
+            await Post.findByIdAndUpdate(req.params.id, { $pull: { likes: req.user._id } });
+            res.status(200).json({ message: 'Post unliked successfully' });
+        } else {
+            // Like post
+            await Post.findByIdAndUpdate(req.params.id, { $push: { likes: req.user._id } });
+            res.status(200).json({ message: 'Post liked successfully' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        console.log('Error in likeUnlikePost: ', error.message);
+    }
+};
+
+export { createPost, getPost, deletePost, likeUnlikePost };
