20065ef18d26da897d3a5d5a6d5aad6565f0ce3c
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-02-20 12:10:56 +0000

    Site: Add edit profile button and follow/unfollow button on user profile

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -12,12 +12,23 @@ import {
     VStack,
     useColorModeValue,
     useToast,
+    Button,
 } from '@chakra-ui/react';
 import { BsInstagram } from 'react-icons/bs';
 import { CgMoreO } from 'react-icons/cg';
+import { useRecoilValue } from 'recoil';
+import userAtom from '../atoms/userAtom';
+import { Link as RouterLink } from 'react-router-dom';
+import { useState } from 'react';
+import useShowToast from '../hooks/useShowToast';
 
 const UserHeader = ({ user }) => {
     const toast = useToast();
+    const currentUser = useRecoilValue(userAtom);
+    const [following, setFollowing] = useState(user.followers.includes(currentUser._id));
+    const showToast = useShowToast();
+    const [updating, setUpdating] = useState(false);
+
     const copyUrl = () => {
         const currentURL = window.location.href;
         navigator.clipboard.writeText(currentURL).then(() => {
@@ -26,6 +37,41 @@ const UserHeader = ({ user }) => {
                 status: 'success',
             });
         });
+    };
+
+    const handleFollowUnfollow = async () => {
+        if (!currentUser) {
+            showToast('Error', 'You must be logged in to follow users', 'error');
+            return;
+        }
+        setUpdating(true);
+        try {
+            const res = await fetch(`/api/users/follow/${user._id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await res.json();
+            if (data.error) {
+                showToast('Error', data.error, 'error');
+                return;
+            }
+
+            if (following) {
+                showToast('Success', 'You have unfollowed ' + user.name, 'success');
+                user.followers.pop(); // simulate removing from followers
+            } else {
+                showToast('Success', 'You have followed ' + user.name, 'success');
+                user.followers.push(currentUser._id); // simulate adding to followers
+            }
+
+            setFollowing(!following);
+        } catch (error) {
+            showToast('Error', error, 'error');
+        } finally {
+            setUpdating(false);
+        }
     };
 
     return (
@@ -68,6 +114,19 @@ const UserHeader = ({ user }) => {
             </Flex>
 
             <Text>{user.bio}</Text>
+
+            {currentUser._id === user._id && (
+                <Link as={RouterLink} to={'/update'}>
+                    <Button size={'sm'}>Edit profile</Button>
+                </Link>
+            )}
+
+            {currentUser._id !== user._id && (
+                <Button size={'sm'} onClick={handleFollowUnfollow} isLoading={updating}>
+                    {following ? 'Unfollow' : 'Follow'}
+                </Button>
+            )}
+
             <Flex w={'full'} justifyContent={'space-between'}>
                 <Flex gap={2} alignItems={'center'}>
                     <Text color={'gray.light'}>{user.followers.length} followers</Text>
